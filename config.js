21771cc16324c815635b248ea4f9942ad763797f
humbertods <36367875+humbertods@users.noreply.github.com>
2026-06-13 07:27:05 -0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.SIRA_CONFIG = {
-  SHEET_URL: 'https://script.google.com/macros/s/AKfycbzyEBabD-2BXhSd1tmIXpWXwzHPWE5CoF4VcGD1c5ILkACl8FmWbQRTL0juM70sxZnw/exec',
-  API_TOKEN: 'sira_2026_nexserv_bridge_7f4c9a'
-};
