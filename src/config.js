d1f2aac3600f16214f98d84a38aab2e5233138a5
humbertods <36367875+humbertods@users.noreply.github.com>
2026-06-17 12:29:16 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 window.SIRA_CONFIG = {
   SHEET_URL: 'https://script.google.com/macros/s/AKfycbzyEBabD-2BXhSd1tmIXpWXwzHPWE5CoF4VcGD1c5ILkACl8FmWbQRTL0juM70sxZnw/exec',
-  API_TOKEN: 'sira_2026_nexserv_bridge_7f4c9a'
+  // Token PÚBLICO de la PWA. Independiente del token del puente (NexServ).
+  API_TOKEN: 'sira_pwa_2026_9e7e11045861'
 };
