48f1f144e3c4446247b97cfef58a52cf78cfd886
humbertods <36367875+humbertods@users.noreply.github.com>
2026-07-03 18:54:38 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 window.SIRA_CONFIG = {
-  SHEET_URL: 'https://script.google.com/macros/s/AKfycbzyEBabD-2BXhSd1tmIXpWXwzHPWE5CoF4VcGD1c5ILkACl8FmWbQRTL0juM70sxZnw/exec',
-  // Token PÚBLICO de la PWA. Independiente del token del puente (NexServ).
-  API_TOKEN: 'sira_pwa_2026_9e7e11045861'
+  SHEET_URL: 'https://script.google.com/macros/s/AKfycbzyEBabD-2BXhSd1tmIXpWXwzHPWE5CoF4VcGD1c5ILkACl8FmWbQRTL0juM70sxZnw/exec'
 };
